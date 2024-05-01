c3f22798072601caff52ee578e2c9c5d932eb5bc
Eldar Khayrullin <eldar.khayrullin@mail.ru>
2024-05-01 10:04:56 +0300

    lineage2game: process multi packets in one tcp segment (in progress)

--- lineage2game.lua
+++ lineage2game.lua
@@ -178,6 +178,8 @@ lineage2game.fields = {
 local xor_key_cache = {}
 local server_xor_key = ""
 local client_xor_key = ""
+-- TODO cache of packet chunks: [pinfo.number] = chunk
+-- TODO chunk - last chunk
 
 ---@param buffer ByteArray
 ---@param isserver boolean
@@ -185,6 +187,7 @@ local client_xor_key = ""
 local function is_encrypted_packet(buffer, isserver)
     local len = packet.length(buffer)
     local opcode = packet.opcode(buffer)
+    -- TODO check current *_xor_key for empty
     if isserver then
         return not (len == 16 and opcode == SERVER_OPCODE.KeyInit)
     else
@@ -207,48 +210,60 @@ local function decode_client_data(tree, opcode, data, isencrypted)
     -- TODO
 end
 
-function lineage2game.dissector(buffer, pinfo, tree)
-    pinfo.cols.protocol = lineage2game.name
-    -- TODO check pinfo.visited
-    -- TODO check pinfo.conversation
-    -- TODO if pinfo.visited then return end
+---@param opcode number
+---@param isserver boolean
+---@return string
+local function opcode_str(opcode, isserver)
+    local str = isserver and SERVER_OPCODE_TXT[opcode] or CLIENT_OPCODE_TXT[opcode]
+    return str and str or ""
+end
 
-    if buffer:len() == 0 then return end
-    -- TODO check buffer:len() and packet length. What to do if not equal?
-    -- FIXME maybe multiple la2 packets in one buffer. Use packet length
+---@param number number pinfo.number
+---@param isserver boolean
+local function process_xor_key_cache(number, isserver)
+    if xor_key_cache[number] then
+        if isserver then
+            server_xor_key = xor_key_cache[number]
+        else
+            client_xor_key = xor_key_cache[number]
+        end
+    else
+        xor_key_cache[number] = isserver and server_xor_key or client_xor_key
+    end
+end
 
-    local isserver = (pinfo.src_port == GAME_PORT)
-    local pf_opcode = isserver and pf_server_opcode or pf_client_opcode
-    local opcode_txt_tbl = isserver and SERVER_OPCODE_TXT or CLIENT_OPCODE_TXT
+---@param key      string Server XOR key
+---@param isserver boolean
+local function init_xor_key(key)
+    server_xor_key = xor.create_key(key, STATIC_XOR_KEY)
+    client_xor_key = server_xor_key
+end
+
+---@param plen number Previous decrypted data length
+---@param isserver boolean
+local function update_xor_key(plen, isserver)
+    if isserver then
+        server_xor_key = xor.next_key(server_xor_key, plen)
+    else
+        client_xor_key = xor.next_key(client_xor_key, plen)
+    end
+end
+
+---@param tree     TreeItem
+---@param buffer   ByteArray
+---@param xor_key  string
+---@param isserver boolean
+local function process_packet(tree, buffer, isserver)
     local isencrypted = is_encrypted_packet(buffer, isserver)
-
-    if not xor_key_cache[pinfo.number] then
-        xor_key_cache[pinfo.number] =
-            isserver and server_xor_key or client_xor_key
-    end
-    local xor_key = xor_key_cache[pinfo.number]
-
-    local subtree = tree:add(lineage2game, buffer(), "Lineage2 Game Protocol")
-    cmn.add_le(subtree, pf_uint16, packet.length_buffer(buffer), "Length", false)
-
-    if isencrypted then
-        local label = "XOR key"
-        local xor_key_tvb = ByteArray.tvb(ByteArray.new(xor_key, true), label)
-        cmn.add_le(subtree, pf_bytes, xor_key_tvb(), label, isencrypted)
-    end
+    -- TODO check isencrypted and *_xor_key is empty then not process. Ret false. Print no XOR key
 
     local opcode_p = nil
     local data_p = nil
+    local xor_key = isserver and server_xor_key or client_xor_key
     if isencrypted then
         -- TODO empty encrypted_block ?
         local dec = xor.decrypt(packet.encrypted_block(buffer), xor_key)
-        -- TODO only not in cache (flag)
-        if isserver then
-            server_xor_key = xor.next_key(xor_key, #dec)
-        else
-            client_xor_key = xor.next_key(xor_key, #dec)
-        end
-
+        -- TODO move down
         local dec_tvb = ByteArray.tvb(ByteArray.new(dec, true), "Decrypted")
 
         opcode_p = packet.decrypted_opcode_buffer(dec_tvb(), isserver)
@@ -258,21 +273,73 @@ function lineage2game.dissector(buffer, pinfo, tree)
         data_p = packet.data_buffer(buffer)
     end
 
+    local opcode = cmn.be(opcode_p)
+
+    -- TODO only not in cache (flag). Check is isencrypted?
+    if isserver and opcode == SERVER_OPCODE.KeyInit then
+        init_xor_key(packet.xor_key(data_p))
+    end
+
+    -- TODO before Decrypted in representation
+    -- TODO add packet id in sequence, example: 1. Opcode name
+    local subtree = tree:add(lineage2game, buffer(), opcode_str(opcode, isserver))
+    cmn.add_le(subtree, pf_uint16, packet.length_buffer(buffer), "Length", false)
+    if isencrypted then
+        local label = "XOR key"
+        local xor_key_tvb = ByteArray.tvb(ByteArray.new(xor_key, true), label)
+        cmn.add_le(subtree, pf_bytes, xor_key_tvb(), label, isencrypted)
+    end
+
+    local pf_opcode = isserver and pf_server_opcode or pf_client_opcode
     cmn.add_be(subtree, pf_opcode, opcode_p, nil, isencrypted)
 
-    local data_st = cmn.generated(tree:add(lineage2game, data_p, "Data"),
+    -- TODO move dec_tvb here
+    local data_st = cmn.generated(subtree:add(lineage2game, data_p, "Data"),
                                   isencrypted)
-
-    local opcode = cmn.be(opcode_p)
-    -- TODO move up
-    if isserver and opcode == SERVER_OPCODE.KeyInit then
-        server_xor_key = xor.create_key(packet.xor_key(data_p), STATIC_XOR_KEY)
-        client_xor_key = server_xor_key
-    end
+    -- TODO decode_data call server or client by isserver
     local decode_data = isserver and decode_server_data or decode_client_data
     decode_data(data_st, opcode, data_p, isencrypted)
 
-    cmn.set_info_field(pinfo, isserver, isencrypted, opcode_txt_tbl[opcode])
+    if isencrypted then
+        update_xor_key(packet.encrypted_block(buffer):len(), isserver)
+    end
+end
+
+function lineage2game.dissector(buffer, pinfo, tree)
+    pinfo.cols.protocol = lineage2game.name
+    -- TODO check pinfo.visited
+    -- TODO check pinfo.conversation
+    -- TODO if pinfo.visited then return end
+
+    if buffer:len() == 0 then return end
+
+    local isserver = (pinfo.src_port == GAME_PORT)
+
+    process_xor_key_cache(pinfo.number, isserver)
+    -- TODO process chunk cache
+
+    local subtree = tree:add(lineage2game, buffer(), "Lineage2 Game Protocol")
+
+    local from = 0
+    while from < buffer:len() do
+        local len = packet.length(buffer(from))
+        if buffer:len() < from + len then
+            -- TODO save chunk and process with next packet (concatenate)
+            print("error: part of packet " .. tostring(pinfo.number))
+            break
+        end
+
+        process_packet(subtree, buffer(from, len), isserver)
+        from = from + len
+    end
+
+    -- local len_warn = (buffer:len() ~= packet.length(buffer)) and " !!!" or " OK"
+    -- TODO move to process_packet
+    -- local opcode_str =
+    --     isserver and SERVER_OPCODE_TXT[opcode] or CLIENT_OPCODE_TXT[opcode]
+    -- if not opcode_str then opcode_str = "" end
+    -- TODO print list of opcode names separated (with stat?) with comma
+    -- cmn.set_info_field(pinfo, isserver, isencrypted, opcode_str .. len_warn)
 end
 
 local tcp_port = DissectorTable.get("tcp.port")
